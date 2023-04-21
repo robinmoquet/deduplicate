e8cc649e639bd28d7598af2a9d2ccd4775fb18f9
Moquet <robin.m07@hotmail.fr>
2023-04-21 12:59:43 +0000

    refactor: improve cache logic

--- exemples/html/src/main.ts
+++ exemples/html/src/main.ts
@@ -9,11 +9,12 @@ const body = {
   };
 const fetcherPost = () => fetch('https://lorem-json.com/api/json', {method: 'POST', body: JSON.stringify(body)}).then(res => res.json())
 const fetcherGet = () => fetch('https://baconipsum.com/api/?type=meat-and-filler').then(res => res.json())
+const fetcherError = () => { throw new Error('fail !') }
 
 async function fetchData() {
-    const { data, error } = await deduplicate('my_key', fetcherGet, 1000)
+    const { data, error } = await deduplicate('my_key', fetcherGet)
     if (error) {
-        console.error(error)
+        console.log(error)
         return
     }
 
@@ -21,11 +22,18 @@ async function fetchData() {
 }
 
 
-console.log(fetchData())
-console.log(fetchData())
-console.log(fetchData())
-console.log(fetchData())
+const data1 = fetchData()
+const data2 = fetchData()
+const data3 = fetchData()
+const data4 = fetchData()
+
+
+console.log(await data1)
+console.log(await data2)
+console.log(await data3)
+console.log(await data4)
+
 
 setTimeout(async () => {
-    console.log(fetchData())
-}, 500)
+    console.log(await fetchData())
+}, 3000)
--- src/core/deduplicate.ts
+++ src/core/deduplicate.ts
@@ -1,24 +1,25 @@
 import { Fetcher, Deduplicate } from './types'
 import { cache } from './cache'
-import { process } from './process'
 
 /**
  * Cf. README.md for exemples an usage explanation 
  */
 export async function deduplicate<Data = unknown>(identifier: string, fetcher: Fetcher<Data>, time: number = 2000): ReturnType<Deduplicate<Data>> {
     try {
-        if (process.get(identifier)) await process.get(identifier)
-        else process.set(identifier, new Promise((resolve) => setTimeout(() => resolve(undefined), time)))
+        if (cache.get(identifier)) return { data: await cache.get(identifier), error: false }
 
-        if (cache.get(identifier)) return { data: cache.get(identifier), error: false }
+        cache.set(identifier, new Promise( async (resolve, reject) => {
+            try {
+                resolve(await fetcher())
+            } catch (e) {
+                reject(e)
+            }
+        }))
 
-        const res = await fetcher()
-        cache.set(identifier, res)
-        process.delete(identifier)
         setTimeout(() => cache.delete(identifier), time)
-
-        return { data: res, error: false }
+        return { data: await cache.get(identifier), error: false }
     } catch (e) {
+        if (cache.get(identifier)) cache.delete(identifier)
         return { data: undefined, error: e }
     }
 }
