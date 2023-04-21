cd446f7370afa1715ec804ed34292520db6aee58
Moquet <robin.m07@hotmail.fr>
2023-04-21 13:53:08 +0000

    fix: clone data for prevent alteration

--- src/core/deduplicate.ts
+++ src/core/deduplicate.ts
@@ -1,12 +1,20 @@
 import { Fetcher, Deduplicate } from './types'
 import { cache } from './cache'
 
+function cloneData<Data = unknown>(data: Data): Data {
+    try {
+        return structuredClone(data)
+    } catch (e) {
+        return data
+    }
+}
+
 /**
  * Cf. README.md for exemples and usage explanation 
  */
 export async function deduplicate<Data = unknown>(identifier: string, fetcher: Fetcher<Data>, time: number = 2000): ReturnType<Deduplicate<Data>> {
     try {
-        if (cache.get(identifier)) return { data: await cache.get(identifier), error: false }
+        if (cache.get(identifier)) return { data: cloneData(await cache.get(identifier)), error: false }
 
         cache.set(identifier, new Promise( async (resolve, reject) => {
             try {
@@ -17,7 +25,7 @@ export async function deduplicate<Data = unknown>(identifier: string, fetcher: F
         }))
 
         setTimeout(() => cache.delete(identifier), time)
-        return { data: await cache.get(identifier), error: false }
+        return { data: cloneData(await cache.get(identifier)), error: false }
     } catch (e) {
         if (cache.get(identifier)) cache.delete(identifier)
         return { data: undefined, error: e }
